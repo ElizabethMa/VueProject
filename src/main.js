1dd7e338b4afd3c3e9707282c43fb2d5632c7a2a
Elizabeth <ma_yanqiong@163.com>
2019-04-12 18:06:41 +0800

    init commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import './plugins/iview.js'
 
 Vue.config.productionTip = false
 
@@ -10,4 +11,4 @@ new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#root-app')
